41d670a15547ae8ff16e50f3c689ef7bb5dfcfed
oskaaditya <oska.aditya18@gmail.com>
2018-11-05 22:13:22 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+    require 'functions.php';
+    $mahasiswa=query("SELECT * FROM mahasiswa")
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Data Mahasiswa</title>
+    </head>
+    <body>
+        <h1>Daftar Mahasiswa</h1>
+
+        <a href="tambah_data.php">Tambah Data Mahasiswa</a>
+        <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Nama</th>
+            <th>Nim</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+            <th>Gambar</th>
+            <th>Aksi</th>
+        </tr>
+        <?php $i=1 ?>
+        <?php foreach($mahasiswa as $row):?>
+        <tr>
+            <td><?= $i; ?></td>
+            <td><?= $row["Nama"]; ?></td>
+            <td><?= $row["Nim"]; ?></td>
+            <td><?= $row["Email"]; ?></td>
+            <td><?= $row["Jurusan"]; ?></td>
+            <td><img src="img/<?php echo $row["Gambar"]; ?>" alt="" scrset="" width="100" height="100"></td>
+            <td>
+                <a href="">Edit</a>
+                <a href="hapus.php?Id=<?php echo $row["Id"]; ?>"onclick="return confirm('Apakah data ini akan dihapus')">Hapus</a>
+            </td>
+        </tr>
+        <?php $i++ ?>
+        <?php endforeach;?>
+        </table>        
+    </body>
+</html>
